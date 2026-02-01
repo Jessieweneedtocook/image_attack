98f8f7d08c163245ab61f96c05bc840f60772278
Jessie Smith <114092829+Jessieweneedtocook@users.noreply.github.com>
2026-02-01 15:42:42 +0000

    Update __init__.py

--- src/img_attack_pkg/__init__.py
+++ src/img_attack_pkg/__init__.py
@@ -1,3 +1,3 @@
-from .pipeline import run_all, run_selected, run_selected2, parse_attack_tokens
+from .pipeline import run_selected_folder, run_selected_file, parse_attack_tokens
 
 __all__ = ["run_selected_file", "run_selected_folder", "parse_attack_tokens"]
